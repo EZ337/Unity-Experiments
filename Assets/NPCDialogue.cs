d0e5616f00bfb9ffb76b7be29106c472ca10746b
EZ <88570645+EZ337@users.noreply.github.com>
2024-07-06 20:46:49 -0400

    Stuff

--- Assets/NPCDialogue.cs
+++ Assets/NPCDialogue.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,36 +6,66 @@ using UnityEngine;
 public class NPCDialogue : MonoBehaviour
 {
     private string message;
-    private bool allowBtnPress = true;
-    public List<string> Dialogues = new List<string>();
+    public bool npcInDialogue = false;
+    public List<ComplexDialogue> Dialogues = new List<ComplexDialogue>();
     public GameObject InteractIcon;
+    private int indx = 0;
 
+    [NaughtyAttributes.Button]
+    public void NextDialogue()
+    {
+        if (indx < Dialogues.Count)
+        {
+            SimpleDialogue.Instance.UpdateText(Dialogues[indx++]);
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         collision.attachedRigidbody.sleepMode = RigidbodySleepMode2D.NeverSleep;
-        allowBtnPress = true;
+        npcInDialogue = false;
         InteractIcon.SetActive(true);
+        indx = 0;
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (allowBtnPress && collision.CompareTag("Player") && Input.GetKey(KeyCode.E))
+        if (npcInDialogue)
         {
-            allowBtnPress = false;
             InteractIcon.SetActive(false);
+        }
 
-
-            // Shows the message
-            SimpleDialogue.Instance.UpdateText(message);
+        if (!npcInDialogue && collision.CompareTag("Player") && Input.GetKey(KeyCode.E))
+        {
+            npcInDialogue = true;
+            NextDialogue();
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         collision.attachedRigidbody.sleepMode = RigidbodySleepMode2D.StartAwake;
-        SimpleDialogue.Instance.EndDialogue();
-        allowBtnPress = true;
-        InteractIcon.SetActive(false);
+        EndDialogue();
     }
+
+    private void EndDialogue()
+    {
+        SimpleDialogue.Instance.EndDialogue();
+        npcInDialogue = false;
+        indx = 0;
+    }
+}
+
+
+[Serializable]
+public class ComplexDialogue
+{
+    // Condition
+    public string LineToSay;
+
+    public static implicit operator string(ComplexDialogue d) { return d.LineToSay; }
 }
